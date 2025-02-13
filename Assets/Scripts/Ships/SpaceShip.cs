6566abc4615fcd364bbb6bf585ec11e0b6072085
JakePhillips-Davies <162450362+JakePhillips-Davies@users.noreply.github.com>
2025-02-13 15:23:57 +0000

    a

--- Assets/Scripts/Ships/SpaceShip.cs
+++ Assets/Scripts/Ships/SpaceShip.cs
@@ -1,7 +1,45 @@
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
-[CreateAssetMenu(fileName = "SpaceShip", menuName = "Scriptable Objects/SpaceShip")]
-public class SpaceShip : ScriptableObject
+[RequireComponent(typeof(Rigidbody))]
+public class SpaceShip : MonoBehaviour
 {
-    
+    //
+    private Vector3 steeringDir;
+    [SerializeField] private float steeringForce;
+
+    private Rigidbody rb;
+
+    private Transform tempTarget = null;
+    //
+
+    private void Start() {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(Random.onUnitSphere * 40000);
+
+        steeringDir = transform.forward;
+    }
+
+    private void Update() {
+        transform.LookAt(Vector3.Lerp(transform.position + transform.forward, transform.position + steeringDir, 0.25f)); // rotate towards velovity lerped
+    }
+
+    private void FixedUpdate() {
+        if (tempTarget == null){ ///!!!!!!!! TEMP !!!!!!!!!!!!!
+            foreach (Asteroid asteroid in FindObjectsByType<Asteroid>(FindObjectsSortMode.None)){
+                if (Random.Range(0, 30) == 2){
+                    tempTarget = asteroid.transform;
+                }
+            }
+        }
+
+        Vector3 desiredDir = tempTarget.position - transform.position;
+
+        steeringDir = Vector3.Normalize(desiredDir - rb.linearVelocity);
+
+        rb.AddForce(steeringDir * steeringForce);
+
+    }
 }
