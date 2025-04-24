7b9669e4a87f22396d339dbd441ee904e3a51c9d
JakePhillips-Davies <162450362+JakePhillips-Davies@users.noreply.github.com>
2025-04-24 01:56:45 +0100

    Update LogAsteroidInRange.cs

--- Assets/Scripts/Ships/ShipAI/Actions/LogAsteroidInRange.cs
+++ Assets/Scripts/Ships/ShipAI/Actions/LogAsteroidInRange.cs
@@ -14,10 +14,9 @@ public class LogAsteroidInRange : Node
             Collider[] asteroidsInRange = Physics.OverlapSphere(ship.transform.position, ship.searchRange, LayerMask.NameToLayer("Asteroid"));
             if (asteroidsInRange.Length == 0) return NodeState.FAILURE;
     
-            int randomCheck = Random.Range(0, asteroidsInRange.Length - 1);
-            if (asteroidsInRange[randomCheck].TryGetComponent<Asteroid>(out Asteroid randomAsteroid))
+            int randomIndex = Random.Range(0, asteroidsInRange.Length);
+            if (asteroidsInRange[randomIndex].TryGetComponent<Asteroid>(out Asteroid randomAsteroid))
                 ship.SetTarget(randomAsteroid.transform);
-            else return NodeState.FAILURE;
 
             return NodeState.SUCCESS;
         }
